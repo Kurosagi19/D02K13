7407d265adfadf8343fc89ec7a864b7586627d29
Kurosagi19 <thanhlanqwerty@gmail.com>
2022-08-24 11:56:21 +0700

    Done

--- src/main/java/OOP/Inherited/Person/Person.java
+++ src/main/java/OOP/Inherited/Person/Person.java
@@ -1,11 +1,5 @@
 package OOP.Inherited.Person;
 
-// Tạo một lớp Person lưu trữ các thông tin sau đây (tên, giới tính, địa chỉ, ngày sinh)
-// Tạo một lớp Student kế thừa từ lớp Person lưu trữ các thôn gtin sau đây (mã sinh viên, điểm trung bình, email)
-// Viết một phương thức nhập thông tin của student
-// Viết một phương thức hiển thị thông tin của student
-// Viết phương trình xem xét có học sinh nào được học bổng không ?
-// Điểm trung bình hơn 8.0 sẽ được học bổng
 
 import java.util.Scanner;
 
@@ -17,7 +11,7 @@ public class Person {
     public String diaChi;
     public String ngaySinh;
     public String maSV;
-    public static int diemTB;
+    public double diemTB;
     public String email;
 
     public String toString() {
@@ -26,6 +20,7 @@ public class Person {
                 "Gioi tinh: " + gioiTinh + '\n' +
                 "Dia chi: " + diaChi + '\n' +
                 "Ngay sinh: " + ngaySinh + '\n' +
-                "Email: " + email + '\n';
+                "Email: " + email + '\n' +
+                "Diem trung binh: " + diemTB + '\n';
     }
 }
--- src/main/java/OOP/Inherited/Person/studentMain.java
+++ src/main/java/OOP/Inherited/Person/studentMain.java
@@ -4,38 +4,33 @@ package OOP.Inherited.Person;
 public class studentMain extends Person {
     static studentMain sv1 = new studentMain();
 
-    public static void nhapThongTin() {
-        System.out.print("Ho va ten: ");
-        sv1.ten = input.nextLine();
-        System.out.print("Gioi tinh: ");
-        sv1.gioiTinh = input.nextLine();
-        System.out.print("Dia chi: ");
-        sv1.diaChi = input.nextLine();
-        System.out.print("Ngay thang nam sinh: ");
-        sv1.ngaySinh = input.nextLine();
-        System.out.print("Ma sinh vien: ");
-        sv1.maSV = input.nextLine();
-        System.out.print("Email: ");
-        sv1.email = input.nextLine();
-    }
-
-    public static void hocBong() {
-        System.out.print("Nhap diem trung binh cua sinh vien: ");
-        diemTB = input.nextInt();
-        if (diemTB >= 8) {
-            System.out.print("Sinh vien du dieu kien nhan hoc bong");
-        } else {
-            System.out.print("Sinh vien khong du dieu kien nhan hoc bong");
-        }
-    }
-    public static void inThongTin() {
-        System.out.println(sv1.toString());
-    }
-
-    public static void main(String[] args) {
+    public void nhapThongTin() {
         System.out.println("Nhap thong tin sinh vien");
-        nhapThongTin();
-        inThongTin();
-        hocBong();
+        System.out.print("Ho va ten: ");
+        ten = input.nextLine();
+        System.out.print("Gioi tinh: ");
+        gioiTinh = input.nextLine();
+        System.out.print("Dia chi: ");
+        diaChi = input.nextLine();
+        System.out.print("Ngay thang nam sinh: ");
+        ngaySinh = input.nextLine();
+        System.out.print("Ma sinh vien: ");
+        maSV = input.nextLine();
+        System.out.print("Email: ");
+        email = input.nextLine();
+        System.out.print("Diem trung binh: ");
+        diemTB = input.nextDouble();
+    }
+
+    public void hocBong() {
+      if (this.diemTB < 8) {
+          System.out.print("Sinh vien khong du dieu kien nhan hoc bong");
+      } else {
+          System.out.print("Sinh vien du dieu kien nhan hoc bong");
+      }
+    }
+
+    public void inThongTin() {
+        System.out.println(super.toString());
     }
 }
