74411d8118dee6a1184b898ae8444c010c7141ce
Kurosagi19 <thanhlanqwerty@gmail.com>
2022-08-24 09:14:12 +0700

    Done

---
+++ src/main/java/OOP/LyThuyet2.java
@@ -0,0 +1,12 @@
+// Tính chất của OOP:
+//  Kế thừa: 2 class kế thừa nhau, class được kế thừa có thuộc tính phương thức nào thì class kế thừa sẽ có thuộc tính phương thức đó
+//           class được kế thừa sẽ không có thuộc tính, phương thức của class kế thừa
+// Có 2 loại kế thừa:
+// - Đơn kế thừa: 1 cha, nhiều con
+//                1 con, 1 cha
+// - Đa kế thừa (không có trong java): 1 cha, nhiều con
+//                                     1 class, nhiều cha
+
+// Đa hình
+// Đóng gói
+// Trừu tượng
