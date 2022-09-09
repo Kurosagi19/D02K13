5c5853dee4a3ab92db66fdb8293be4c784030fcc
Kurosagi19 <thanhlanqwerty@gmail.com>
2022-09-09 11:45:12 +0700

    Done

--- src/main/java/WriterReader/main.java
+++ src/main/java/WriterReader/main.java
@@ -6,13 +6,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class main {
+    private static String file;
+
     public static void main(String[] args) throws IOException {
-        try{
+        try {
             FileReader fileReader = new FileReader("C:\\Users\\TôLânThành\\OneDrive - bkacad.edu.vn\\Documents\\Lap trinh Java\\demo.txt");
 //            FileWriter fileWriter = new FileWriter("C:\\Users\\TôLânThành\\OneDrive - bkacad.edu.vn\\Documents\\Lap trinh Java\\demo.txt");
             int i;
             while ((i = fileReader.read()) != -1) {
-                System.out.print((char)i);
+                System.out.print((char) i);
             }
             System.out.println(fileReader.read());
             fileReader.close();
@@ -22,5 +24,6 @@ public class main {
             fileWriter.close();
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("Khong tim thay file");
+        }
     }
 }
